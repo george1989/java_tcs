069c371487bdb5e39e79791940634839af1b470e
jlcastro <jlcastro@pichincha.com>
2022-03-31 19:08:26 -0500

    fuentes iniciales

---
+++ src/test/java/com/pichincha/java_tcs/JavaTcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.java_tcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
